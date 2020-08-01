a17bb8bb702d4a56fa682d6ce2cc1de5333a92f3
aman7895 <aman.middha@github.com>
2020-08-01 18:36:17 +0800

    Initial commit with project setup

---
+++ src/main/java/deep/aman/checkoutsystem/domain/Product.java
@@ -0,0 +1,12 @@
+package deep.aman.checkoutsystem.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Product {
+    private String name;
+    private Double price;
+
+}
